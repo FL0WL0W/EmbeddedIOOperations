97ee9fc9ce9eb2cc18e806c8bcbb608a7a1a739e
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2021-03-22 15:38:07 -0500

    Operation_GetTickTests

---
+++ test/src/Operation_GetTickTests.cpp
@@ -0,0 +1,40 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "Operations/Operation_GetTick.h"
+#include "MockTimerService.h"
+using namespace testing;
+using namespace EmbeddedIOServices;
+using namespace OperationArchitecture;
+
+namespace UnitTests
+{
+	class Operation_GetTickTests : public ::testing::Test 
+	{
+		protected:
+		IOperationBase *_operation;
+		MockTimerService _timerService;
+		EmbeddedIOServiceCollection _embeddedIOServiceCollection;
+		unsigned int _size = 0;
+
+		Operation_GetTickTests() 
+		{
+			_embeddedIOServiceCollection.TimerService = &_timerService;
+
+			_operation = Operation_GetTick::Create(&_embeddedIOServiceCollection, 0, _size);
+		}
+	};
+
+	TEST_F(Operation_GetTickTests, ConfigsAreCorrect)
+	{
+		ASSERT_EQ(0, _size);
+	}
+
+	TEST_F(Operation_GetTickTests, WhenGettingValues_ThenCorrectValueIsReturned)
+	{
+		EXPECT_CALL(_timerService, GetTick()).Times(1).WillOnce(Return(0));
+		ASSERT_EQ(0, _operation->Execute<uint32_t>());
+
+		EXPECT_CALL(_timerService, GetTick()).Times(1).WillOnce(Return(500));
+		ASSERT_EQ(500, _operation->Execute<uint32_t>());
+	}
+}
