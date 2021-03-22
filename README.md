eba31fb80448ab307db4d7e442fbdc57cd3fae11
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2021-03-22 15:42:54 -0500

    Operation_PwmPinWriteTests

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # EmbeddedIOOperations
 Embedded Operations for the Operation Architecture
-<br/><br/>
-TODO<br/>
-Operation_PwmPinWriteTests
---
+++ test/src/Operation_PwmPinWriteTests.cpp
@@ -0,0 +1,62 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "Operations/Operation_PwmPinWrite.h"
+#include "MockPwmService.h"
+using namespace testing;
+using namespace EmbeddedIOServices;
+using namespace OperationArchitecture;
+
+namespace UnitTests
+{
+	class Operation_PwmPinWriteTests : public ::testing::Test 
+	{
+		protected:
+		MockPwmService _pwmService;
+		EmbeddedIOServiceCollection _embeddedIOServiceCollection;
+		IOperationBase *_operation;
+        unsigned int _size = 0;
+
+		Operation_PwmPinWriteTests() 
+		{
+			_embeddedIOServiceCollection.PwmService = &_pwmService;
+
+			void *config = malloc(sizeof(uint16_t) + sizeof(uint16_t));
+			void *buildConfig = config;
+
+			//pin 1
+			*((uint16_t *)buildConfig) = 1;
+			buildConfig = (void *)(((uint16_t *)buildConfig) + 1);
+
+			//minFrequency 2
+			*((uint16_t *)buildConfig) = 2;
+			buildConfig = (void *)(((uint16_t *)buildConfig) + 1);
+
+			EXPECT_CALL(_pwmService, InitPin(1, Out, 2)).Times(1);
+			_operation = Operation_PwmPinWrite::Create(&_embeddedIOServiceCollection, config, _size);
+		}
+	};
+
+	TEST_F(Operation_PwmPinWriteTests, ConfigsAreCorrect)
+	{
+		ASSERT_EQ(4, _size);
+	}
+
+	TEST_F(Operation_PwmPinWriteTests, WhenWritingValue_ThenCorrectValueIsWritten)
+	{
+		PwmValue pwmValue = { 0.1f, 0.05f };
+		EXPECT_CALL(_pwmService, WritePin(1, pwmValue)).Times(1);
+		_operation->Execute(pwmValue.Period, pwmValue.PulseWidth);
+
+		pwmValue = { 0.025f, 0.004f };
+		EXPECT_CALL(_pwmService, WritePin(1, pwmValue)).Times(1);
+		_operation->Execute(pwmValue.Period, pwmValue.PulseWidth);
+
+		pwmValue = { 0.05f, 0.04f };
+		EXPECT_CALL(_pwmService, WritePin(1, pwmValue)).Times(1);
+		_operation->Execute(pwmValue.Period, pwmValue.PulseWidth);
+
+		pwmValue = { 0.05333333333333f, 0.04f };
+		EXPECT_CALL(_pwmService, WritePin(1, pwmValue)).Times(1);
+		_operation->Execute(pwmValue.Period, pwmValue.PulseWidth);
+	}
+}
