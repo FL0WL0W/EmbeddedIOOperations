6dc4e243f4812878d43142d1a9db45266b79351d
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2021-08-11 01:51:57 -0500

    update

--- src/Operations/EmbeddedIOOperationFactoryRegister.cpp
+++ src/Operations/EmbeddedIOOperationFactoryRegister.cpp
@@ -1,5 +1,4 @@
 #include "Operations/EmbeddedIOOperationFactoryRegister.h"
-#include "CreateWithParameters.h"
 #include "Operations/Operation_AnalogPinRead.h"
 #include "Operations/Operation_DigitalPinRead.h"
 #include "Operations/Operation_DigitalPinRecord.h"
@@ -20,17 +19,17 @@ namespace OperationArchitecture
 {
     void EmbeddedIOOperationFactoryRegister::Register(uint32_t idOffset, OperationFactory *factory, const EmbeddedIOServiceCollection *embeddedIOServiceCollection)
     {
-        factory->Register(idOffset + 1, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_AnalogPinRead::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 2, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_DigitalPinRead::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 3, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_DigitalPinRecord::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 4, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_DutyCyclePinRead::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 5, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_FrequencyPinRead::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 6, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_PulseWidthPinRead::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 7, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_DigitalPinWrite::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 8, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_PwmPinWrite::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 9, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_GetTick::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 10, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_SecondsToTicks::Create, embeddedIOServiceCollection));
-        factory->Register(idOffset + 11, new CreateWithParameters<const EmbeddedIOServiceCollection *>(Operation_TicksToSeconds::Create, embeddedIOServiceCollection));
+        factory->Register(idOffset + 1, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_AnalogPinRead::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 2, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_DigitalPinRead::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 3, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_DigitalPinRecord::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 4, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_DutyCyclePinRead::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 5, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_FrequencyPinRead::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 6, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_PulseWidthPinRead::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 7, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_DigitalPinWrite::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 8, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_PwmPinWrite::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 9, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_GetTick::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 10, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_SecondsToTicks::Create(config, sizeOut, embeddedIOServiceCollection); });
+        factory->Register(idOffset + 11, [embeddedIOServiceCollection](const void *config, size_t &sizeOut) { return Operation_TicksToSeconds::Create(config, sizeOut, embeddedIOServiceCollection); });
     }
 }
 
